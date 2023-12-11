ec45e025cfc32f603f660b621301343c068ac08a
Abdalla Sarhan <abdalla1@my.yorku.ca>
2023-12-11 16:25:30 -0500

    function for pushing notifications using expo

---
+++ index.js
@@ -0,0 +1,19 @@
+import Expo from "expo-server-sdk";
+
+exports.sendPushNotification = (req, res) => {
+  const expo = new Expo();
+  const token = "ExponentPushToken[9rKWfpKNoxctQIn7jGRK5v]";
+
+  try {
+    expo.sendPushNotificationsAsync([
+      {
+        to: token,
+        title: "expo notification test",
+        body: "from node js server",
+      },
+    ]);
+    res.status(200).send("Notification Sent!");
+  } catch (error) {
+    res.status(500).send("Error Sending Notification");
+  }
+};
